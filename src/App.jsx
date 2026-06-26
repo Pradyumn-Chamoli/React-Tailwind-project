56b76c6d38e59cc54dc3b6c88c2566209f9aa97a
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2026-06-27 00:54:18 +0530

    Added hero and Trusstedby component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/navbar'
+import Hero from './components/Hero';
+import TrustedBy from './components/TrustedBy';
 
 const App = () => {
 
@@ -7,6 +9,8 @@ const App = () => {
   return (
     <div className='dark:bg-black relative'>
       <Navbar  theme={theme} setTheme={setTheme}/>
+      <Hero/>
+      <TrustedBy/>
     </div>
   )
 }
